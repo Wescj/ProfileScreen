97382d56bc779ae84b10b3ee9531f594c901e3fc
Wescj <44911617+Wescj@users.noreply.github.com>
2023-08-23 18:08:49 -0700

    issue with 3 columns

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ export default function App() {
     const newImage = {
       id: images.length + 1,
       url: `https://picsum.photos/200/300?random=${images.length + 1}`,
+      username: `user`,
     };
 
     setImages([...images, newImage]);
@@ -17,9 +18,9 @@ export default function App() {
 
   useEffect(() => {
     // Load initial images when the component mounts
+    setImages([]);
     addImage();
-    addImage();
-    addImage();
+    console.log("Ran image additionsss")
   }, []);
 
   const renderSmallPf = ({ item }) => (
@@ -28,19 +29,31 @@ export default function App() {
       source={{ uri: item.url }}
       style={styles.subPFPic}
       />
-      <Text>Name</Text>
+      <Text style={styles.subPFText}>username {item.username}
+      </Text>
     </View>
+  );
+
+  const renderPhoto = ({ item }) => (
+    <View>
+      <Image
+        source={{ uri: item.url }}
+        style={styles.gridPhoto}
+      />
+    </View>
+
   );
 
   return (
     <View style={styles.container}>
-      {/* <Text>{toString(data[0])+"   dagga"}</Text> */}
+      
       <Image
         source={{ uri: 'https://picsum.photos/200/300' }}
         style = {styles.profilePic}
       />
-
+      <Text style={styles.pfText}>Username</Text>
       <View style = {styles.friendsContainer}>
+        <Text style={styles.pfText}>Your Friends</Text>
         <FlatList
           data={images}
           renderItem={renderSmallPf}
@@ -48,7 +61,16 @@ export default function App() {
           horizontal
         />
       </View>
-      
+
+      <View style={styles.imageContainer}>
+        <FlatList
+            data={images}
+            renderItem={renderPhoto}
+            keyExtractor={item => item.id.toString()}
+            numColumns={3}
+        />
+        <Text>safsaffs</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -66,13 +88,13 @@ const styles = StyleSheet.create({
     height: "25%",
     aspectRatio: 1, 
     borderRadius: 1000,
-    // borderWidth: 1,
   },
-  subPFPic: {
-    height: '75%',
-    aspectRatio: 1, 
-    borderRadius: 1000,
-    // borderWidth: 1,
+
+  pfText:{
+    fontWeight: 'bold', // Make text bold
+    fontSize: 25, // Adjust font size as needed
+    marginVertical: 10, // Add spacing after the FlatList
+    marginHorizontal: 20
   },
 
   subPFContainer:{
@@ -80,13 +102,34 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
 
-  // subPFText:{
+  subPFPic: {
+    height: '60%',
+    aspectRatio: 1, 
+    borderRadius: 1000,
+  },
 
-  // }
+  subPFText:{
+    // fontWeight: 'bold', // Make text bold
+    fontSize: 15, // Adjust font size as needed
+    marginTop: 10, // Add spacing after the FlatList
+  },
 
   friendsContainer: {
     height: '30%',
-    flexDirection: 'row', // Display items in a row
-    alignItems: 'center', // Center items vertically
+    // flexDirection: 'row', // Display items in a row
+    // alignItems: 'center', // Center items vertically
+    backgroundColor: '#eeeeee',
+    borderRadius: 25,
+    // paddingHorizontal:10, 
+  },
+  imageContainer:{
+    width: '100%',
+    alignItems:'center',
+  },
+
+  gridPhoto:{
+    height: undefined,
+    width: '50%',
+    aspectRatio: 1, 
   }
 });
