2fb5aa5348a7cb67e088bd68cc740dc80f503c8f
Wescj <44911617+Wescj@users.noreply.github.com>
2023-08-23 17:25:36 -0700

    friends list done

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList} from 'react-native';
+import React, { useEffect, useState } from 'react';
+
 
 export default function App() {
+  const [images, setImages] = useState([]);
+
+  const addImage = () => {
+    const newImage = {
+      id: images.length + 1,
+      url: `https://picsum.photos/200/300?random=${images.length + 1}`,
+    };
+
+    setImages([...images, newImage]);
+  };
+
+  useEffect(() => {
+    // Load initial images when the component mounts
+    addImage();
+    addImage();
+    addImage();
+  }, []);
+
+  const renderSmallPf = ({ item }) => (
+    <View style= {styles.subPFContainer}>
+      <Image
+      source={{ uri: item.url }}
+      style={styles.subPFPic}
+      />
+      <Text>Name</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>{toString(data[0])+"   dagga"}</Text> */}
+      <Image
+        source={{ uri: 'https://picsum.photos/200/300' }}
+        style = {styles.profilePic}
+      />
+
+      <View style = {styles.friendsContainer}>
+        <FlatList
+          data={images}
+          renderItem={renderSmallPf}
+          keyExtractor={item => item.id.toString()}
+          horizontal
+        />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +59,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: '20%'
   },
+  profilePic: {
+    width: undefined,
+    height: "25%",
+    aspectRatio: 1, 
+    borderRadius: 1000,
+    // borderWidth: 1,
+  },
+  subPFPic: {
+    height: '75%',
+    aspectRatio: 1, 
+    borderRadius: 1000,
+    // borderWidth: 1,
+  },
+
+  subPFContainer:{
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+
+  // subPFText:{
+
+  // }
+
+  friendsContainer: {
+    height: '30%',
+    flexDirection: 'row', // Display items in a row
+    alignItems: 'center', // Center items vertically
+  }
 });
